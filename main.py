458fa4658647af8f95d69195b80bde8c908a0f2d
Pasindu Akalpa <pasinduakalpa1998@gmail.com>
2023-04-09 03:32:39 +0530

    ⚡️ Add basic API funcs and Update Response Model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from io import BytesIO
 import os
 import json
+import base64
+import threading
 from model import ResponseModel
 from PIL import Image
 from fastapi import FastAPI, Security, HTTPException, status, File, Depends
@@ -12,18 +14,27 @@ from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from DETECTION.detection import TEXT_DETECTION
 from RECOGNITION.recognition import TEXT_RECOGNITION
+from FIREBASE.firebaseIO import FirebaseIO
 from configs import Config
 
 load_dotenv(".env.development")
-API_KEY: str = os.environ.get("API_KEY")
-REDIRECT_URL: str = os.environ.get("REDIRECT_URL")
-CORS_ORIGINS: str = os.environ.get("CORS_ORIGINS")
+API_KEY: str = os.environ.get(str(Config.API_KEY.name))
+REDIRECT_URL: str = os.environ.get(str(Config.REDIRECT_URL.name))
+CORS_ORIGINS: str = os.environ.get(str(Config.CORS_ORIGINS.name))
+FIREBASE_DATABASE_URL: str = os.environ.get(
+    str(Config.FIREBASE_DATABASE_URL.name))
+FIREBASE_STORAGE_BUCKET_URL: str = os.environ.get(
+    str(Config.FIREBASE_STORAGE_BUCKET_URL.name))
+FIREBASE_KEY_ENCODED: str = os.environ.get(str(Config.FIREBASE_KEY.name))
+FIREBASE_KEY_DECODED = base64.b64decode(FIREBASE_KEY_ENCODED).decode('UTF-8')
+FIREBASE_KEY_JSON = json.loads(FIREBASE_KEY_DECODED)
 
 api_key_header = APIKeyHeader(name="x-api-key", auto_error=False)
 
 detection_model = TEXT_DETECTION()
 recognition_model = TEXT_RECOGNITION()
-
+fb = FirebaseIO(FIREBASE_KEY_JSON, FIREBASE_DATABASE_URL,
+                FIREBASE_STORAGE_BUCKET_URL)
 
 app = FastAPI(title="Prescription Extractor", redoc_url=None)
 
@@ -74,22 +85,64 @@ def read_root():
         return RedirectResponse(REDIRECT_URL)
 
 
-@app.post("/detect", status_code=200)
+@app.post("/detect_img", status_code=200)
 async def add_post(api_key: APIKey = Depends(get_api_key), file: bytes = File(...)) -> ResponseModel:
     response = ResponseModel()
     try:
         input_image = Image.open(BytesIO(file)).convert("RGB")
-        detected_dict = await detection_model.detect(input_image)
-        response.confidences = await detected_dict[Config.CONF_LIST.value]
+        detected_dict = detection_model.detect(input_image)
+        crop_dict = detection_model.crop_image()
+        url_and_name = fb.uploadImage(detected_dict[Config.IMAGE.value])
+        documentId = fb.createDocument(url_and_name[0], url_and_name[1])
+        conf_list = detected_dict[Config.CONF_LIST.value]
+
+        # Create Thread for background process
+        thread = threading.Thread(
+            target=runRecognizerInBackground, args=(documentId, crop_dict, conf_list,), daemon=True)
+        thread.start()
+
+        # Create Response
+        response.documentID = documentId
+        response.imageURL = url_and_name[0]
+        response.boxes = crop_dict[Config.CROP_XYXY.value]
+        response.confidences = conf_list
         resJson = jsonable_encoder(response)
         return JSONResponse(
             status_code=status.HTTP_200_OK,
             content=resJson
         )
-    except:
+    except Exception as e:
+        print(e)
         response.error = "INVALID MEDIA TYPE"
         resJson = jsonable_encoder(response)
         return JSONResponse(
             status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
             content=resJson
         )
+
+
+def runRecognizerInBackground(documentId: str, crop_dict: dict, conf_list: list) -> None:
+    """
+    #### Runs an OCR recognizer on a set of image crops in the background and updates a Firebase Firestore document with the recognition results.
+
+    Arguments:
+    - documentId (str): The ID of the Firestore document to update with the recognition results.
+    - crop_dict (dict): A dictionary containing image crops and their corresponding bounding box coordinates, as returned by an object detection model.
+    - conf_list (list[float]): A list of confidence scores for the detected objects.
+
+    Returns:
+        None.
+
+    Notes:
+    - This function runs an OCR recognizer on each image crop in the input dictionary in the background, without blocking the main thread. The recognition results are stored in a list, which is then passed to the FirebaseClient.updateDocument() method along with the confidence scores and bounding box coordinates.
+    - The OCR recognizer is assumed to be defined in a separate module or class and imported as `recognition_model`. The recognizer should take a NumPy array representing an image as input and return a string representing the recognized text.
+    - The input `crop_dict` should be a dictionary with keys 'CROP_IMG' and 'CROP_XYXY', corresponding to the image crops and their bounding box coordinates, respectively. The image crops should be provided as a list of NumPy arrays, and the coordinates should be provided as a list of lists, in the format [[x1, y1, x2, y2], [x1, y1, x2, y2], ...]. The `conf_list` should be a list of confidence scores for the detected objects, in the same order as the image crops.
+    - The function assumes that the FirebaseClient instance is defined in the global scope as `fb`, and that the updateDocument() method is available on the instance.
+    - This function does not return any values, but updates the specified Firestore document with the recognition results.
+    """
+    text_list = []
+    for image in crop_dict[Config.CROP_IMG.value]:
+        recognized_text = recognition_model.recognize(image)
+        text_list.append(recognized_text)
+        fb.updateDocument(documentId, text_list, conf_list,
+                          crop_dict[Config.CROP_XYXY.value])
--- model.py
+++ model.py
@@ -7,7 +7,7 @@ class ResponseModel(BaseModel):
     #### Response model for the endpoint
     """
     documentID: str = None
-    imagePath: str = None
+    imageURL: str = None
     boxes: List[str] = []
     confidences: List[str] = []
     error: str = None
